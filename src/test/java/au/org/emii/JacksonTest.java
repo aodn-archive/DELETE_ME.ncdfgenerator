466f6e1cac05a87abf2af2a2549614cdfde338c3
julian1 <julian.atkinson71@gmail.com>
2015-04-17 16:42:50 +1000

    update

--- src/test/java/au/org/emii/JacksonTest.java
+++ src/test/java/au/org/emii/JacksonTest.java
@@ -19,6 +19,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 
+import com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector; 
+
+
+
+
+
+
+class Simple {
+    public int x = 1;
+    public int y = 2;
+}
+
+
 public class JacksonTest 
 {
 
@@ -34,7 +47,11 @@ public class JacksonTest
 	public void testIntegerLiteral() throws Exception
 	{
 
-		System.out.println( "\n@@@@@\n\n Jackson test" ); 
+		ObjectMapper xmlMapper = new XmlMapper();
+		Simple value = xmlMapper.readValue("<Simple><x>1</x><y>2</y></Simple>", Simple.class);
+
+
+		System.out.println( "**** fuck " + value   );
 	}
 
 
