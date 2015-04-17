648a68960b3c9e92ae3b109a42bd1d18080b9480
julian1 <julian.atkinson71@gmail.com>
2015-04-17 18:40:32 +1000

    ok, worked but without base class

--- src/test/java/au/org/emii/Jackson2Test.java
+++ src/test/java/au/org/emii/Jackson2Test.java
@@ -10,18 +10,44 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.Properties;
 
+import java.util.ArrayList;
+
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonCreator;
 
 
+import au.org.emii.ncdfgenerator.IDimension;
 import au.org.emii.ncdfgenerator.DimensionImpl;
 
 
 
 // https://github.com/FasterXML/jackson-annotations
 // http://www.cowtowncoder.com/blog/archives/2011/07/entry_457.html
+
+/*
+   @JsonCreator
+   public Simple2(@JsonProperty("x") int x, @JsonProperty("y") int y) {
+*/ 
+
+class VariableEncoder 
+{
+	final String				variableName;
+	final ArrayList<DimensionImpl>	dimensions; // change name childDimensions
+
+
+   @JsonCreator
+	public VariableEncoder(
+		@JsonProperty("name") String variableName,
+		@JsonProperty("dimensions") ArrayList< DimensionImpl> dimensions
+	) {
+		this.variableName = variableName;
+		this.dimensions = dimensions;
+	}
+
+}
+
 
 
 
@@ -48,6 +74,23 @@ public class Jackson2Test
 		assertTrue( value != null ); 
 		System.out.println( "**** DIMESNION " + value   );
 	}
+
+	// what about multiple dimensions...
+	// needs to be like this ... 
+
+	// final ArrayList<IDimension>	dimensions; // change name childDimensions
+	
+	@Test
+	public void test03() throws Exception
+	{
+		ObjectMapper xmlMapper = new XmlMapper();
+		VariableEncoder value = xmlMapper.readValue("<?xml version=\"1.0\"?><VariableEncoder><dimensions> <DimensionImpl name=\"TIME\"/> </dimensions>  </VariableEncoder>", VariableEncoder.class);
+		assertTrue( value != null ); 
+		System.out.println( "**** VAR ENCODER " + value   );
+	}
+
+
+
 
 }	
 
