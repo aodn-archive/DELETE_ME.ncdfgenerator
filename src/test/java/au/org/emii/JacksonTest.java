cc9b4aaeab4a345c81a283577e91df37dbdbe995
julian1 <julian.atkinson71@gmail.com>
2015-04-17 17:06:57 +1000

    constructor example

--- src/test/java/au/org/emii/JacksonTest.java
+++ src/test/java/au/org/emii/JacksonTest.java
@@ -17,6 +17,12 @@ import java.io.InputStream ;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+
+// import com.fasterxml.jackson.annotate.JsonProperty;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 /*
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonAnySetter;
@@ -24,15 +30,39 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 
-import com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector; 
 */
 
 
+
+// jackson annotations not recognized...
+
+// https://github.com/FasterXML/jackson-annotations
+
+
+class CtorPOJO 
+{
+   private final int _x, _y;
+
+   @JsonCreator
+   public CtorPOJO(@JsonProperty("x") int x, @JsonProperty("y") int y) {
+      _x = x;
+      _y = y;
+   }
+}
 
 
 
 class Simple 
 {
+	//public Simple( int x1, int x2)
+	public Simple( )
+	{
+		System.out.println( "my constructor" ); 
+	}
+
+
+//	@XmlElement(required = true)
+//	@JsonProperty("fuck")
     public int x = 1;
     public int y = 2;
 }
@@ -50,18 +80,23 @@ public class JacksonTest
 
 
 	@Test
-	public void testIntegerLiteral() throws Exception
+	public void test01() throws Exception
 	{
-
 		ObjectMapper xmlMapper = new XmlMapper();
 		Simple value = xmlMapper.readValue("<?xml version=\"1.0\"?> <Simple><x>1</x><y>2</y></Simple>", Simple.class);
+		System.out.println( "**** fuck " + value   );
+	}
 
-
+	@Test
+	public void test02() throws Exception
+	{
+		ObjectMapper xmlMapper = new XmlMapper();
+		CtorPOJO value = xmlMapper.readValue("<?xml version=\"1.0\"?> <CtorPOJO><x>1</x><y>2</y></CtorPOJO>", CtorPOJO.class);
 		System.out.println( "**** fuck " + value   );
 	}
 
 
-
-
 }	
+
+
 
