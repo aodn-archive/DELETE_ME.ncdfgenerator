6ff78054feadf9870fe7a895e986cbc756eaeaff
julian1 <julian.atkinson71@gmail.com>
2015-05-27 17:22:16 +1000

    update

--- src/main/java/au/org/emii/ncdfgenerator/IOutputFormatter.java
+++ src/main/java/au/org/emii/ncdfgenerator/IOutputFormatter.java
@@ -5,7 +5,8 @@ import java.io.OutputStream;
 
 public interface IOutputFormatter {
 
+    // should we be returning bool ? 
     void write(String filename, InputStream is) throws Exception;
-    void close() throws Exception;
+    // void close() throws Exception; // why do we need this...
 }
 
